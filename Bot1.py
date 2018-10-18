2e8170a40afa6717bf9c6b92b8164ebc8237561d
Camila <camreyaro@alum.us.es>
2018-10-18 18:03:29 +0200

    testing..

--- Bot1.py
+++ Bot1.py
@@ -135,7 +135,7 @@ def main():
     
     dp.add_handler(CommandHandler('start', start))
 
-    dp.add_handler(MessageHandler(Filters.passport_data, msg))
+    dp.add_handler(MessageHandler(Filters.text, msg))
 
     # log all errors
     dp.add_error_handler(error)
