6161f93ea7bce4620a2a6a02d7ad057b92eb201e
Camila <camreyaro@alum.us.es>
2018-10-18 18:06:56 +0200

    testing..

--- Bot1.py
+++ Bot1.py
@@ -135,7 +135,7 @@ def main():
     
     dp.add_handler(CommandHandler('start', start))
 
-    dp.add_handler(MessageHandler(Filters.text, msg))
+    dp.add_handler(MessageHandler(Filters.all, msg))
 
     # log all errors
     dp.add_error_handler(error)
